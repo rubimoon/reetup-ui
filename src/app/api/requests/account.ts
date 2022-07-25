a90b64c90947b75f3646d9ffefd549790d342623
Ruby Chang <dev.rubimoon@gmail.com>
2022-07-25 11:45:29 +0900

    chore

--- src/app/api/requests/account.ts
+++ src/app/api/requests/account.ts
@@ -6,8 +6,6 @@ export const Account = {
   login: (user: UserFormValues) => requests.post<User>("/account/login", user),
   register: (user: UserFormValues) =>
     requests.post<User>("/account/register", user),
-  fbLogin: (accessToken: string) =>
-    requests.post<User>(`/account/fbLogin?accessToken=${accessToken}`, {}),
   refreshToken: () => requests.post<User>("/account/refreshToken", {}),
   verifyEmail: (token: string, email: string) =>
     requests.post<void>(
